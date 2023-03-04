a9f0bbdbcbd810c9e9724f7f39cae7e84ef0c3b4
NullSEC <48302509+Cr7pt0nic@users.noreply.github.com>
2023-03-03 21:33:02 -0800

    Update networkan.py

--- networkan.py
+++ networkan.py
@@ -26,27 +26,32 @@ def analyze_packet(packet):
             ip_count[src_ip] = {"count": 1, "time": time.time()}
         
         # Detecting potential DDOS attack
-        if ip_count[src_ip]["count"] > 500:
+        if ip_count[src_ip]["count"] > 50:
             print("\033[31m" + f"DDOS attack detected from {src_ip}:{src_port} to {dst_ip}:{dst_port}" + "\033[0m")
+            # Block the attacker's IP address
+            os.system(f"iptables -A INPUT -s {src_ip} -j DROP")
             
-    current_time = time.time()
-    ip_count = {ip: count for ip, count in ip_count.items() if current_time - count["time"] <= time_frame}
+        current_time = time.time()
+        ip_count = {ip: count for ip, count in ip_count.items() if current_time - count["time"] <= time_frame}
     
-    print(ip_count)
-
     # Injection attack detection
     if packet.haslayer(Raw):
         payload = packet[Raw].load.decode('utf-8', 'ignore')
         if "SELECT" in payload.upper() or "DROP" in payload.upper():
             print("\033[31m" + f"Potential SQL injection attack from {src_ip}:{src_port} to {dst_ip}:{dst_port}" + "\033[0m")
+            # Block the attacker's IP address
+            os.system(f"iptables -A INPUT -s {src_ip} -j DROP")
         if "<script>" in payload.lower():
             print("\033[31m" + f"Potential XSS attack from {src_ip}:{src_port} to {dst_ip}:{dst_port}" + "\033[0m")
         
-        # Brute forcing detection
-        if flags == "PA":
-            payload = packet[TCP].payload.load.decode('utf-8', 'ignore')
-            if len(payload) > 10 and len(set(payload)) == 1:
-                print("\033[31m" + f"Potential brute forcing from {src_ip}:{src_port} to {dst_ip}:{dst_port}" + "\033[0m")
+    # Brute forcing detection
+    if flags == "PA":
+        payload = packet[TCP].payload.load.decode('utf-8', 'ignore')
+        if len(payload) > 10 and len(set(payload)) == 1:
+            print("\033[31m" + f"Potential brute forcing from {src_ip}:{src_port} to {dst_ip}:{dst_port}" + "\033[0m")
+            # Block the attacker's IP address
+            os.system(f"iptables -A INPUT -s {src_ip} -j DROP")
 
 # Sniffing packets on the network interface
 sniff(filter="tcp", prn=analyze_packet)
+
